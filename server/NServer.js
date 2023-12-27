93b432c3ce9e6147c2fedc8d58a458c3d35315b7
Raja Singh <rajasgh18@gmail.com>
2023-12-28 00:29:29 +0530

    MySQL Test connection

--- server/NServer.js
+++ server/NServer.js
@@ -2,6 +2,7 @@ const express = require("express");
 const App = express();
 const CORS = require("cors");
 const BodyParser = require('body-parser');
+const DB = require('./db');
 const auth = require("./routes/auth/index");
 
 App.use(CORS());
@@ -14,6 +15,15 @@ App.use(BodyParser.urlencoded({ extended: false }));
 
 //Routes
 App.use("/auth", auth);
+App.use('/', (req, res)=>{
+  DB.query('SELECT * FROM Users', (error, results)=>{
+    if(error){
+      res.status(500).send('Error retrieving data from the Databse');
+      return;
+    }
+    res.status(200).json(results);
+  })
+})
 
 App.listen(5000, () => {
   console.log("Dristi Server Running at http://localhost:5000");
