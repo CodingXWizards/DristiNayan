f8c0d002cbca24d4963a4df39ad149110a6a9ee3
Raja Singh <rajasgh18@gmail.com>
2023-12-28 22:50:06 +0530

    Added Automatic creation of Users table if does not exits

--- server/db.js
+++ server/db.js
@@ -18,6 +18,16 @@ connection.connect((err) => {
   console.log('Connected to MySQL Database!');
 });
 
+connection.query("SHOW TABLES", (err, result) => {
+  let tableExists = result.filter(table => table['Tables_in_' + process.env.MYSQL_DATABASE] == "Users");
+
+  //If Users Table does exists it will return else will create it.
+  if (tableExists.length != 0) return;
+
+  connection.query("CREATE TABLE Users(Name VARCHAR(20) NOT NULL, UserID VARCHAR(20) NOT NULL PRIMARY KEY, Password VARCHAR(20) NOT NULL, Phone INT(10) UNIQUE)")
+  console.log("Users Table Create as it didn't existed!");
+})
+
 //  CREATE TABLE Users(Name VARCHAR(20) NOT NULL, UserID VARCHAR(20) NOT NULL PRIMARY KEY, Password VARCHAR(20) NOT NULL, Phone INT(10) UNIQUE);
 
 module.exports = connection;
