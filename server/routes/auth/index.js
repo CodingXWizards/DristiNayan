b2159f6786653ef6d192f819c96219905d5d9dc9
Raja Singh <rajasgh18@gmail.com>
2024-01-01 01:05:18 +0530

    Fixed signup existing user issue

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -14,25 +14,23 @@ Router
         body('userId', "Please enter a valid id").isLength({ min: 3 }),
         body('password', "Please enter a password more than 5 letters").isLength({ min: 5 }),
     ], async (req, res) => {
-        console.log(req.body);
         try {
             let errors = validationResult(req);
             if (!errors.isEmpty()) return res.status(400).json({ erros: errors.array() })
-            mysql.query("SELECT * FROM Users WHERE UserID = ?", [req.body.userId], (err, result) => {
+            mysql.query("SELECT * FROM Users WHERE UserID = ?", [req.body.userId], async (err, result) => {
                 if (err)
                     res.status(500).json({ "Error": err });
                 console.log(result)
-                if (!result) return res.status(400).send("A User exists with this UserID")
-            })
-            const salt = await bcrypt.genSalt(10);
-            const secPassword = await bcrypt.hash(req.body.password, salt);
-
-            mysql.query("INSERT INTO Users(Name, UserID, Password) VALUES (?, ?, ?)", [req.body.name, req.body.userId, secPassword], (err, result) => {
-                if (err)
-                    return res.status(500).json({ "Error:": err });
-                const data = { user: { userid: req.body.userId } };
-                const authToken = JWT.sign(data, secretKey);
-                res.status(200).json({ authToken });
+                if (result.length != 0) return res.status(400).send("A User exists with this UserID")
+                const salt = await bcrypt.genSalt(10);
+                const secPassword = await bcrypt.hash(req.body.password, salt);
+                mysql.query("INSERT INTO Users(Name, UserID, Password) VALUES (?, ?, ?)", [req.body.name, req.body.userId, secPassword], (err, result) => {
+                    if (err)
+                        return res.status(500).json({ "Error:": err });
+                    const data = { user: { userid: req.body.userId } };
+                    const authToken = JWT.sign(data, secretKey);
+                    res.status(200).json({ user: { name: req.body.name, userID: req.body.userId, password: secPassword }, authToken });
+                })
             })
 
         } catch (error) {
@@ -54,8 +52,8 @@ Router
 
                 const data = { user: { id: user.UserID } };
                 const authToken = JWT.sign(data, secretKey);
-                const {Password, ...others} = user;
-                res.status(200).json({ 'user':others, authToken });
+                const { Password, ...others } = user;
+                res.status(200).json({ user: others, authToken });
             })
         } catch (error) {
             console.error(error);
