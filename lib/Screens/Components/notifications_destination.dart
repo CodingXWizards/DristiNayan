0ea7fa9129a003b09ee54b4d16e0895933caec7b
Raja Singh <rajasgh18@gmail.com>
2024-01-05 00:05:50 +0530

    Optimized Notification Screen

--- lib/Screens/Components/notifications_destination.dart
+++ lib/Screens/Components/notifications_destination.dart
@@ -13,42 +13,33 @@ class NotificationsDestination extends StatefulWidget {
 class _NotificationsDestinationState extends State<NotificationsDestination> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
+    return const SingleChildScrollView(
       child: Padding(
-        padding: const EdgeInsets.all(20),
+        padding: EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text(
-              "Notifications",
-              style: GoogleFonts.firaSans(
-                  fontSize: 28, color: TailwindColors.blueGray.shade600),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const NotificationTile(
+            NotificationTile(
               text:
                   "Scheduled power outage affecting Station A this Sunday from 1 AM to 5 AM. Please plan your operations accordingly",
             ),
-            const NotificationTile(
+            NotificationTile(
               text:
                   "Suspicious activity reported near the cargo area. Kindly remain vigilant and report any unusual incidents to the authorities",
             ),
-            const NotificationTile(
+            NotificationTile(
               text:
                   "Severe thunderstorms expected in the region. Take necessary precautions and ensure passenger safety during boarding and disembarking",
             ),
-            const NotificationTile(
+            NotificationTile(
               text:
                   "Ensure all safety protocols are followed during maintenance checks on the tracks. Your adherence is crucial for a secure operation",
             ),
-            const NotificationTile(
+            NotificationTile(
               text:
                   "Expect increased footfall during the weekend festival. Coordinate with security for efficient crowd management and safety measures",
             ),
-            const NotificationTile(
+            NotificationTile(
               text:
                   "New feature 'Incident Report' now available on the app. Easily report any issues or emergencies for swift resolution",
             ),
@@ -82,7 +73,7 @@ class NotificationTile extends StatelessWidget {
         Icon(
           Icons.notifications,
           color: TailwindColors.blueGray.shade600,
-          size: 32,
+          size: 24,
         ),
         const SizedBox(
           width: 16,
@@ -90,7 +81,7 @@ class NotificationTile extends StatelessWidget {
         Flexible(
           child: Text(
             text,
-            style: GoogleFonts.firaSans(color: TailwindColors.blueGray),
+            style: GoogleFonts.firaSans(color: TailwindColors.blueGray, fontSize: 12),
           ),
         )
       ]),
