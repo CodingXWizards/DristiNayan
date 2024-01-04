8c60c1c801a7bd094a8091d045b7aaf0195b6a07
Raja Singh <rajasgh18@gmail.com>
2024-01-05 01:44:52 +0530

    Realtime basic layout

--- lib/Screens/realtime.dart
+++ lib/Screens/realtime.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:flutter_tailwindcss_defaults/colors.dart";
+import "package:google_fonts/google_fonts.dart";
 
 class Realtime extends StatefulWidget {
   final VoidCallback toggleSecondBody;
@@ -14,12 +16,86 @@ class Realtime extends StatefulWidget {
 class _Realtime extends State<Realtime> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-          onPressed: () {
-            widget.toggleSecondBody();
-          },
-          child: const Text("Realtime")),
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                InkWell(
+                  onTap: () => widget.toggleSecondBody(),
+                  child: Icon(
+                    Icons.arrow_back_rounded,
+                    color: TailwindColors.blueGray.shade600,
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Text(
+                  "Real-Time Crowd Monitoring",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.raleway(
+                      fontSize: 20,
+                      color: TailwindColors.blueGray.shade600),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                children: List.generate(4, (index) => CameraBox(index: index+1,)),
+              ),
+            ),
+            // const CameraBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CameraBox extends StatelessWidget {
+  final int index;
+  const CameraBox({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 4),
+      width: size.width * 0.4,
+      height: size.width * 0.4 + 20,
+      child: Column(
+        children: [
+          Text(
+            "Camera $index",
+            style: GoogleFonts.firaSans(fontSize: 16),
+          ),
+          const SizedBox(height: 8),
+          Container(
+            width: size.width * 0.4,
+            height: size.width * 0.4 - 20,
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(8)),
+              color: TailwindColors.blueGray.shade300,
+              boxShadow: [
+                BoxShadow(
+                    blurRadius: 12,
+                    spreadRadius: 2,
+                    color: TailwindColors.blueGray.shade200)
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
