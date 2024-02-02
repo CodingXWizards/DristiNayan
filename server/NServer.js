020397e6339b85ef7b657a116406c1600d019437
Raja Singh <rajasgh18@gmail.com>
2024-02-02 21:58:54 +0530

    route for criminal added in main server file

--- server/NServer.js
+++ server/NServer.js
@@ -4,20 +4,22 @@ const CORS = require("cors");
 const BodyParser = require('body-parser');
 const DB = require('./db');
 const auth = require("./routes/auth/index");
+const criminal = require("./routes/criminal");
 
 App.use(CORS());
 App.use(express.json());
 
 // Parse application/json
-App.use(BodyParser.json());
+App.use(BodyParser.json({ limit: '100mb' }));
 // Parse application/json
-App.use(BodyParser.urlencoded({ extended: false }));
+App.use(BodyParser.urlencoded({ extended: false, limit: '50mb' }));
 
 //Routes
 App.use("/auth", auth);
-App.use('/', (req, res)=>{
-  DB.query('SELECT * FROM Users', (error, results)=>{
-    if(error){
+App.use("/criminal", criminal);
+App.use('/', (req, res) => {
+  DB.query('SELECT * FROM Users', (error, results) => {
+    if (error) {
       res.status(500).send('Error retrieving data from the Databse');
       return;
     }
